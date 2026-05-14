5785da1371a733ead465f638c940de0460051cec
Rayy <selormtormeti@gmail.com>
2026-05-14 03:54:07 -0700

    Add intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+// My Introduction
+const name = "Tormeti Raymond Selorm";
+const age = 20; // change to your actual age
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
