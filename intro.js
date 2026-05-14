10a0b247316a8124b8efe61945da9949e1d8e00b
Rayy <selormtormeti@gmail.com>
2026-05-14 04:18:09 -0700

    message

--- intro.js
+++ intro.js
@@ -1,6 +1,6 @@
 // My Introduction
 const name = "Tormeti Raymond Selorm";
-const age = 20; // change to your actual age
+const age = 20;
 const favoriteLanguage = "JavaScript";
 
 console.log("Name:", name);
