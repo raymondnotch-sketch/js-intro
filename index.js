25cdc0b3deef281d0b8b68a124e4740faabed045
Rayy <selormtormeti@gmail.com>
2026-05-16 12:37:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = "Boahemaa Dwamena Rosemary";
+const age = 23;
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
